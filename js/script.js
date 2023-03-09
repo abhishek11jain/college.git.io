1f3397a7352fdac9f76ea2d5314f166d53b1fe79
abhishek11jain <jainabhishekjain1107@gmail.com>
2023-03-09 22:56:25 +0530

    Commit

--- js/script.js
+++ js/script.js
@@ -181,6 +181,30 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-LLWL5N9CSM');
 
+
+// -----------------------------
+
+let modalplace = document.querySelectorAll('#ModalSection .carousel .carousel-item');
+modalplace.forEach((el) => {
+  const minPerSlide = 1
+  let next = el.nextElementSibling
+  for (var i=1; i<minPerSlide; i++) {
+    if (!next) {
+      next = modalplace[0]
+    }
+    let cloneChild = next.cloneNode(true)
+    el.appendChild(cloneChild.children[0])
+    next = next.nextElementSibling
+  }
+})
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-LLWL5N9CSM');
+
+
+
+
 // Less And More
 function viewMore() {
 	var dots = document.getElementById("show");
